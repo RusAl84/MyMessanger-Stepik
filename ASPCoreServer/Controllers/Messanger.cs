dff4eca26c26d70a052518238acf0ebdb2cd17d2
Rusal84 <rusal@bk.ru>
2021-03-24 21:52:40 +0300

    Создал ASPServer

---
+++ ASPCoreServer/Controllers/Messanger.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPCoreServer.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class Messanger : ControllerBase
+  {
+
+    // GET api/<Messanger>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "Hochu v VDV " + id.ToString();
+    }
+
+    // POST api/<Messanger>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+
+  }
+}
