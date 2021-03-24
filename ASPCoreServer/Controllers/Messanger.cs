f57df551b26f580bbeb6b34e391229b3b374e412
Rusal84 <rusal@bk.ru>
2021-03-24 22:10:21 +0300

    добавил контроллер на сервер

--- ASPCoreServer/Controllers/Messanger.cs
+++ ASPCoreServer/Controllers/Messanger.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyMessanger_Stepik;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,18 +14,32 @@ namespace ASPCoreServer.Controllers
   [ApiController]
   public class Messanger : ControllerBase
   {
-
+    static List<Message> ListOfMessages = new List<Message>();
     // GET api/<Messanger>/5
     [HttpGet("{id}")]
     public string Get(int id)
     {
-      return "Hochu v VDV " + id.ToString();
+      string OutputString = "Not found";
+      if ((id < ListOfMessages.Count) && (id >= 0))
+      {
+        OutputString = JsonConvert.SerializeObject(ListOfMessages[id]);
+      }
+      Console.WriteLine(String.Format("Запрошено сообщение № {0} : {1}", id, OutputString));
+      return OutputString;
     }
 
     // POST api/<Messanger>
     [HttpPost]
-    public void Post([FromBody] string value)
+    public IActionResult SendMessage([FromBody] Message msg)
     {
+      if (msg == null)
+      {
+        return BadRequest();
+      }
+      ListOfMessages.Add(msg);
+      Console.WriteLine(String.Format("Всего сообщений: {0} Посланное сообщение: {1}", ListOfMessages.Count, msg));
+      //return new NoContentResult();
+      return new OkResult();
     }
 
 
