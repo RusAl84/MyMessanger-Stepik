ed6f66fc0fd25841b8408f0ab294bf846775a02b
Rusal84 <rusal@bk.ru>
2021-03-24 23:17:08 +0300

    Создание WIndows Forms

---
+++ WindowsFormsClient/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WindowsFormsClient
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(661, 364);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(127, 74);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.ItemHeight = 16;
+      this.listBox1.Location = new System.Drawing.Point(12, 12);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(776, 324);
+      this.listBox1.TabIndex = 1;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(13, 364);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(371, 22);
+      this.textBox1.TabIndex = 2;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(13, 408);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(625, 22);
+      this.textBox2.TabIndex = 3;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.listBox1);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+  }
+}
+
