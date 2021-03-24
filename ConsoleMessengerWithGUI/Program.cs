5ee424df25241260d4106645551a4089fee0ac75
Rusal84 <rusal@bk.ru>
2021-03-25 00:16:19 +0300

    start TerminalGui

---
+++ ConsoleMessengerWithGUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyMessanger_Stepik;
+using System;
+using System.Collections.Generic;
+using Terminal.Gui;
+
+namespace ConsoleMessengerWithGUI
+{
+  class Program
+  {
+    // Очередь сообщений
+    private static List<Message> messages = new List<Message>();
+    private static MessangerClientAPI API = new MessangerClientAPI();
+
+    private static MenuBar menu;
+    private static Window winMain;
+    private static Window winMessages;
+    private static Label labelUser;
+    private static TextField fieldUsername;
+    private static Label labelMessage;
+    private static TextField fieldMessage;
+    private static Button btnSend;
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
